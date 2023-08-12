9e480e3b3dd609edd993e329e8d58b2944c306fd
Francisco Moretti <franciscoemoretti@gmail.com>
2023-08-12 14:45:43 +0100

    feat(initial component code): initial code of the react component of this package

---
+++ src/index.tsx
@@ -0,0 +1,68 @@
+import React, {
+  cloneElement,
+  isValidElement,
+  ReactElement,
+  ReactNode,
+} from 'react';
+import { slug } from 'github-slugger';
+
+import { cn } from 'mxcn';
+
+interface AutoLinkHeadingProps {
+  children: ReactNode;
+  className: string;
+  linkClassName?: string;
+  ariaLabel?: string;
+  headingId?: string;
+}
+
+function isHeadingElement(element: ReactNode): element is ReactElement {
+  return (
+    isValidElement(element) &&
+    typeof element.type === 'string' &&
+    /^h[1-6]$/.test(element.type)
+  );
+}
+
+export function AutoLinkHeading({
+  children,
+  className,
+  linkClassName = '',
+  ariaLabel = 'Link to section',
+  headingId = '',
+}: AutoLinkHeadingProps): JSX.Element | null {
+  const firstChild = React.Children.toArray(children)[0];
+
+  if (!isHeadingElement(firstChild)) {
+    console.warn('AutoLinkHeader: The first child is not a heading element.');
+    return null;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const firstGrandChild = React.Children.toArray(firstChild.props.children)[0];
+
+  if (firstGrandChild === undefined) {
+    console.warn(
+      'AutoLinkHeader: No text content found for the header element.'
+    );
+    return null;
+  }
+
+  const headerSlug = headingId || slug(firstGrandChild.toString());
+  const clonedFirstChild = cloneElement(firstChild, {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    className: cn(firstChild.props.className, className),
+    id: headerSlug,
+    children: (
+      <a
+        href={`#${headerSlug}`}
+        className={linkClassName}
+        aria-label={ariaLabel}
+      >
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
+        {firstChild.props.children}
+      </a>
+    ),
+  });
+  return clonedFirstChild;
+}
